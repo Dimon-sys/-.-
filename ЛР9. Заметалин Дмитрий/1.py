0d0ddc0ae2eb9429ae5909e64d4afafbb64ffb39
Dimon-sys <136633867+Dimon-sys@users.noreply.github.com>
2025-12-17 20:44:03 +0300

    Delete ЛР9. Заметалин Дмитрий directory

--- ЛР9. Заметалин Дмитрий/1.py
+++
@@ -1,5 +0,0 @@
-#Вариант10
-print('Данные вводить через пробел')
-x = [int(i) for i in input('x = ').split(' ')]
-y = [int(i) for i in input('y = ').split(' ')]
-print(sum[(2/(x[i]+y[i])) for i in range(len(x))])
--- ЛР9. Заметалин Дмитрий/2.py
+++
@@ -1,4 +0,0 @@
-#Вариант10
-x = [int(i) for i in input('Вводить элементы через пробел: ').split(' ')]
-s = [k for k in x if k <= x[0]]
-print(sum(s)/len(s))
