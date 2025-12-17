2669c627cc14a294f09e317bc4bc49d524eb4bef
Dimon-sys <136633867+Dimon-sys@users.noreply.github.com>
2025-12-17 20:43:47 +0300

    Delete ЛР7. Заметалин Дмитрий directory

--- ЛР7. Заметалин Дмитрий/1.py
+++
@@ -1,5 +0,0 @@
-#Вариант10
-s = input('строка= ')
-m = int(input('m='))
-n = int(input('n='))
-print(s[m-1:n+1])
--- ЛР7. Заметалин Дмитрий/2.py
+++
@@ -1,8 +0,0 @@
-#Вариант10
-s = input().lower().replace(' ','').replace(',','').replace(':','').replace('-','').replace(';','')
-c = 1
-for i in range(len(s)-1):
-    if s[i] == s[i+1]:
-        c += 1
-print(c)
-        
