96b40aa034c03433585cf6fb3053cf933f47fdb2
Dimon-sys <136633867+Dimon-sys@users.noreply.github.com>
2025-12-17 20:37:49 +0300

    Delete ЛР. Программирование на Python/ЛР10 directory

--- ЛР. Программирование на Python/ЛР10/3.py
+++
@@ -1,8 +0,0 @@
-from random import *
-n = int(input('Размер массива: '))
-a = [[randint(-10, 10) for _ in range(n)] for __ in range(n)]
-
-print(*[i for i in a], sep='\n')
-
-print('Сумма элементов двумерного массива - ', sum([sum(i) for i in a]))
-print('Количество строк, в пределах каждой из которых элементы упорядочены по возрастанию - ', len([x for x in a if x.sort() == x]))
