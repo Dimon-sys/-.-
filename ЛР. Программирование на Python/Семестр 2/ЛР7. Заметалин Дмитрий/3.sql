3169c11dece8aff3254fa7e9c390f8b4005e5ac3
Dimon-sys <136633867+Dimon-sys@users.noreply.github.com>
2026-04-23 12:32:52 +0300

    Update 3.sql

--- ЛР. Программирование на Python/Семестр 2/ЛР7. Заметалин Дмитрий/3.sql
+++ ЛР. Программирование на Python/Семестр 2/ЛР7. Заметалин Дмитрий/3.sql
@@ -12,5 +12,4 @@ FROM sales sl
 INNER JOIN merch m ON sl.merch_id = m.id
 INNER JOIN sellers s ON sl.seller_id = s.id
 INNER JOIN departments d ON s.department_id = d.id
-WHERE m.id = 3 
-ORDER BY sl.date;
+WHERE m.id = 3;
